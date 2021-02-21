8e67cbf18a1461fee983ab9745202a8d28e6b3e7
FlaviaCohen <flaviacohen1988@gmail.com>
2021-02-21 16:07:25 -0300

    configuración inicial

---
+++ epayco/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../sass/footer.scss";
+
+const Footer = () => {
+
+    return(
+        <div className="footer-container">
+            <p className="text">ePayco © 2011 - 2019 todos los derechos reservados.</p>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ epayco/src/containers/Home/Home.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../../sass/home.scss";
+
+const Home = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.eu/rest/v2/all")
+      .then((res) => console.log(res));
+  }, []);
+
+  return (
+    <div className="home-container">
+      <p className="title">Información del formulario</p>
+      <p>
+        Ingrese el título y la descripción que visualizarán los usuarios durante
+        el proceso de pago
+      </p>
+      <form className="form">
+        <div className="row">
+          <div className="field-container">
+            <label>Nombre</label>
+            <input></input>
+          </div>
+          <div className="field-container">
+            <label>Apellido</label>
+            <input></input>
+          </div>
+        </div>
+        <div className="row">
+          <div className="field-container">
+            <label>Seleccione un país</label>
+            <select></select>
+          </div>
+          <div className="field-container">
+            <label>Número de documento</label>
+            <input></input>
+          </div>
+        </div>
+        <div className="btns">
+          <button>Enviar</button>
+          <button>Cancelar</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Home;

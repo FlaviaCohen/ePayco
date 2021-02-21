be695a99dcdfad7adf77e1566c629137b6a3ac7f
FlaviaCohen <flaviacohen1988@gmail.com>
2021-02-21 17:46:22 -0300

    Formulario controlado

---
+++ epayco/src/components/FormModal.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const FormModal = () => {
+    return(
+        <div>Modal is Open</div>
+    )
+}
+
+export default FormModal
--- epayco/src/containers/Home/Home.js
+++ epayco/src/containers/Home/Home.js
@@ -1,15 +1,42 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import Modal from "react-modal";
+import FormModal from "../../components/FormModal.js"
 import "../../sass/home.scss";
 
 const Home = () => {
   const [countries, setCountries] = useState([]);
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [country, setCountry] = useState("");
+  const [document, setDocument] = useState("");
+  const [modal, setModal] = useState(false);
+  const [success, setSucces] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name && surname && country && document) {
+      setSucces(true);
+    }
+    setModal(true);
+  };
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSurname = (e) => {
+    setSurname(e.target.value);
+  };
+
+  const handleDocument = (e) => {
+    setDocument(e.target.value);
+  };
 
   useEffect(() => {
     axios
       .get("https://restcountries.eu/rest/v2/all")
-      .then((res) => setCountries(res.data.slice(0,20)));
+      .then((res) => setCountries(res.data.slice(0, 20)));
   }, []);
 
   return (
@@ -22,31 +49,55 @@ const Home = () => {
       <form className="form">
         <div className="row">
           <div className="field-container">
-            <label>Nombre <p>*</p></label>
-            <input></input>
+            <label>
+              Nombre <p>*</p>
+            </label>
+            <input onChange={handleName} value={name}></input>
           </div>
           <div className="field-container">
-            <label>Apellido <p>*</p></label>
-            <input></input>
+            <label>
+              Apellido <p>*</p>
+            </label>
+            <input onChange={handleSurname} value={surname}></input>
           </div>
         </div>
         <div className="row">
           <div className="field-container">
-            <label>Seleccione un país <p>*</p></label>
-            <select>
-              {countries && countries.length ? countries.map(country => <option key={country.alpha2Code}>{country.name}</option>) : null}
+            <label>
+              Seleccione un país <p>*</p>
+            </label>
+            <select onChange={(e) => setCountry(e.target.value)}>
+              {countries && countries.length
+                ? countries.map((country) => (
+                    <option key={country.alpha2Code} value={country.name}>
+                      {country.name}
+                    </option>
+                  ))
+                : null}
             </select>
           </div>
           <div className="field-container">
-            <label>Número de documento <p>*</p></label>
-            <input></input>
+            <label>
+              Número de documento <p>*</p>
+            </label>
+            <input onChange={handleDocument} value={document}></input>
           </div>
         </div>
         <div className="btns">
-          <button className="send-btn"><p>enviar</p></button>
-          <button className="cancel-btn"><p>Cancelar</p></button>
+          <button className="send-btn" onClick={handleSubmit}>
+            <p>enviar</p>
+          </button>
+          <button className="cancel-btn">
+            <p>Cancelar</p>
+          </button>
         </div>
       </form>
+      <Modal
+        isOpen={modal}
+        success={success}
+        /* className="abono-modal"
+        overlayClassName="overlay" */
+      />
     </div>
   );
 };

e6ac7b3c2ada247845d85f2e0d11a541695f89c7
FlaviaCohen <flaviacohen1988@gmail.com>
2021-02-21 17:09:16 -0300

    Estilo Sidebar

--- epayco/src/components/Sidebar.js
+++ epayco/src/components/Sidebar.js
@@ -1,12 +1,25 @@
 import React from "react";
+import logo1 from "../imgs/logo1.svg";
+import iconHouse from "../imgs/icon-house.svg";
+import iconUser from "../imgs/icon-user.svg";
 
 const Sidebar = () => {
-
-    return(
-        <div className="sidebar-container">
-            
-        </div>
-    )
-}
+  return (
+    <div className="sidebar-container">
+      <img className="logo" src={logo1} />
+      <div className="user-name">
+        <p>ALEXANDER CEBALLOS</p>
+      </div>
+      <div className="section">
+          <img src={iconHouse}/>
+          <p>Dashboard</p>
+      </div>
+      <div className="section">
+          <img src={iconUser}/>
+          <p>Clientes</p>
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;

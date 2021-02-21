1dd54d1e4a156e10f10564948bf18edf95fec8a3
FlaviaCohen <flaviacohen1988@gmail.com>
2021-02-21 17:28:07 -0300

    Estilo Footer y detalles formulario + requeset paises

--- epayco/src/components/Footer.js
+++ epayco/src/components/Footer.js
@@ -1,13 +1,19 @@
 import React from "react";
-import "../sass/footer.scss";
+import logo2 from "../imgs/logo2.svg";
+import logo3 from "../imgs/logo3.svg";
 
 const Footer = () => {
-
-    return(
-        <div className="footer-container">
-            <p className="text">ePayco © 2011 - 2019 todos los derechos reservados.</p>
-        </div>
-    )
-}
+  return (
+    <div className="footer-container">
+      <p className="text">
+        ePayco © 2011 - 2019 todos los derechos reservados.
+      </p>
+      <div className="logos-container">
+        <img src={logo2} />
+        <img src={logo3} />
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
--- epayco/src/containers/Home/Home.js
+++ epayco/src/containers/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+
 import "../../sass/home.scss";
 
 const Home = () => {
@@ -8,7 +9,7 @@ const Home = () => {
   useEffect(() => {
     axios
       .get("https://restcountries.eu/rest/v2/all")
-      .then((res) => console.log(res));
+      .then((res) => setCountries(res.data.slice(0,20)));
   }, []);
 
   return (
@@ -21,21 +22,23 @@ const Home = () => {
       <form className="form">
         <div className="row">
           <div className="field-container">
-            <label>Nombre</label>
+            <label>Nombre <p>*</p></label>
             <input></input>
           </div>
           <div className="field-container">
-            <label>Apellido</label>
+            <label>Apellido <p>*</p></label>
             <input></input>
           </div>
         </div>
         <div className="row">
           <div className="field-container">
-            <label>Seleccione un país</label>
-            <select></select>
+            <label>Seleccione un país <p>*</p></label>
+            <select>
+              {countries && countries.length ? countries.map(country => <option key={country.alpha2Code}>{country.name}</option>) : null}
+            </select>
           </div>
           <div className="field-container">
-            <label>Número de documento</label>
+            <label>Número de documento <p>*</p></label>
             <input></input>
           </div>
         </div>
